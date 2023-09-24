4831feccf10ac9f7560fabc733fc2c57b50300dc
Messey A Ndiomo <messey.bilal@gmail.com>
2023-09-24 03:47:38 +0100

    thirteenth deployment commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,48 @@
 import { Carousel } from "react-bootstrap";
-import About from "./About";
-import Work from "./Work";
-import Contact from "./Contact";
 
 const Home = () => {
   return (
     <main className="vh-100 mt-md-0">
       <Carousel>
-        <Carousel.Item interval={2000}>{<About />}</Carousel.Item>
-        <Carousel.Item interval={2000}>{<Work />}</Carousel.Item>
-        <Carousel.Item interval={2000}>{<Contact />}</Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-2.png")
+                : require("../imgs/slide-2.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-3.png")
+                : require("../imgs/slide-3.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-4.png")
+                : require("../imgs/slide-4.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
       </Carousel>
     </main>
   );
