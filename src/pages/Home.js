c171a73a4c76de2d57f72e41d00d0f5d650e4515
Messey A Ndiomo <messey.bilal@gmail.com>
2023-09-26 06:56:01 +0100

    twenty-first deployment commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,45 @@ const Home = () => {
   return (
     <main className="vh-100 mt-md-0">
       <Carousel>
-        <Carousel.Item interval={2000}>{<About />}</Carousel.Item>
-        <Carousel.Item interval={2000}>{<Work />}</Carousel.Item>
-        <Carousel.Item interval={2000}>{<Contact />}</Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-2.png")
+                : require("../imgs/slide-2.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-3.png")
+                : require("../imgs/slide-3.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            src={
+              window.innerWidth < 769
+                ? require("../imgs/slide-sm-4.png")
+                : require("../imgs/slide-4.png")
+            }
+            className="img-fluid"
+            alt=""
+            width="100%"
+            height="auto"
+          />
+        </Carousel.Item>
       </Carousel>
     </main>
   );
