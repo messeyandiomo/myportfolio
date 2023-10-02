d1d46d28b9c86ebbd6e8a85116cd7d900cccd41b
Messey A Ndiomo <messey.bilal@gmail.com>
2023-10-02 19:22:08 +0100

    twenty-eighth commit: updating of module ContactModal and deleting of ContactModal.css

--- src/pages/ContactModal.js
+++ src/pages/ContactModal.js
@@ -1,14 +1,37 @@
-import { Modal } from "react-bootstrap";
-import "./ContactModal.css";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-const ContactModal = (props) => {
+function ContactModal(props) {
+  const [show, setShow] = useState(true);
+
   return (
-    <Modal show={true} className="d-flex align-items-center contactModal">
-      <Modal.Body>
+    <Modal
+      show={show}
+      backdrop={"static"}
+      className="d-flex align-items-center"
+    >
+      <Modal.Body
+        className="text-center rounded"
+        style={{
+          backgroundColor: "#282c34",
+          borderBottom: "0 none",
+          color: "white",
+        }}
+      >
         <p>{props.contactModalMessage}</p>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            setShow(false);
+            window.location.reload();
+          }}
+        >
+          Close
+        </Button>
       </Modal.Body>
     </Modal>
   );
-};
+}
 
 export default ContactModal;
